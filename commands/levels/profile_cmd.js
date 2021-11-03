bfe02a8967bedb02f0a6b40b842c06d0b20e6fca
Haubruge Pierre-Yves <46069694+Piwy-dev@users.noreply.github.com>
2021-11-03 12:40:59 +0100

    correction du bug du Ds_Store

--- commands/levels/profile_cmd.js
+++ commands/levels/profile_cmd.js
@@ -4,13 +4,13 @@ const language = require('../../language')
 module.exports = {
     commands: ['p', 'profile'],
     callback: async (message) => {
-      const {guild} = message
-      const target = message.mentions.users.first() || message.author
-  
-      const guildId = message.guild.id
-      const userId = target.id
-      const xp = await profile.addxp(guildId, userId, 0)
-  
-      message.channel.send(`${target} ${language(guild, "HAS")} ${xp} points`)
+        const { guild } = message
+        const target = message.mentions.users.first() || message.author
+
+        const guildId = message.guild.id
+        const userId = target.id
+        const xp = await profile.addxp(guildId, userId, 0)
+
+        message.channel.send(`${target} ${language(guild, "HAS")} ${xp} points`)
     },
-  }
+}
