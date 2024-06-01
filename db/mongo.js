fe03bcf8125b61bab81f4d1e2abbc5144b952f4d
Pierre-Yves Haubruge <pierre-yves.haubruge@student.uclouvain.be>
2024-06-01 14:03:38 +0200

    chage db connection uri

--- db/mongo.js
+++ db/mongo.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 
 module.exports = async() => {
-    await mongoose.connect(process.env.MONGOPATH, {
+    const mongo_user = encodeURIComponent(process.env.MONGO_USER);
+    const mongo_password = encodeURIComponent(process.env.MONGO_PASSWORD);
+    const mongo_db = process.env.MONGO_DB;
+    const mongo_uri = `mongodb+srv://${mongo_user}:${mongo_password}@cluster0.rbdrg.mongodb.net/${mongo_db}?retryWrites=true&w=majority`;
+    
+    const options = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
-    })
-    return mongoose
+    }
+
+    await mongoose.connect(mongo_uri, options);
+    return mongoose;
 }
